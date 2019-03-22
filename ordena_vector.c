169f76e3a002aa2526aab1b95566f2fedee68d70
Oriol <oriolalascercos@gmail.com>
2019-03-22 10:56:31 +0100

    Acabats els fprintf's

--- ordena_vector.c
+++ ordena_vector.c
@@ -7,6 +7,8 @@ void insertion_sort(int n);
 void agafar_nombres(FILE* fitxer_ansi);
 int agafar_primers();
 int es_primer(int num);
+int mirar_anterior(int i, int j);
+int equal_to_0(int j);
 void claus_criptografiques(int num_max);
 void show(int num, int numbers[]);
 
@@ -27,6 +29,7 @@ int main(int argc, char **argv)
     fclose(fitxer_ansi);
     insertion_sort(MAX);
     num_primers = agafar_primers();
+    fprintf(fitxer, "%d %p\n", 3, &num_primers);
     claus_criptografiques(num_primers);
     printf("Vector ordenat:\n");
     show(MAX, nombres);
@@ -43,12 +46,21 @@ void agafar_nombres(FILE* fitxer_ansi)
 {
     char nombre[4];
     int i;
-    fprintf(fitxer, "%d %p", 2, &i);
-    fprintf(fitxer, "%d %p", 3, &i);
+    fprintf(fitxer, "%d %p\n", 3, &i);
+    fprintf(fitxer, "%d %p\n", 2, &i);
+    fprintf(fitxer, "%d %p\n", 2, &i);
     for(i = 0; i < MAX; i++)
     {
         fscanf(fitxer_ansi, "%s", nombre);
+        fprintf(fitxer, "%d %p\n", 3, &nombre);
         nombres[i] = atoi(nombre);
+        fprintf(fitxer, "%d %p\n", 2, &nombre);
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &nombres[i]);
+
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &i);
+        fprintf(fitxer, "%d %p\n", 2, &i);
     }
 
 }
@@ -56,16 +68,50 @@ void agafar_nombres(FILE* fitxer_ansi)
 void insertion_sort(int n)
 {
     int i, key, j;
+
+    fprintf(fitxer, "%d %p\n", 3, &i);
+    fprintf(fitxer, "%d %p\n", 2, &i);
+    fprintf(fitxer, "%d %p\n", 2, &n);
     for(i = 1; i < n; i++)
     {
         key = nombres[i];
+        fprintf(fitxer, "%d %p\n", 3, &key);
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 2, &nombres[i]);
+
+
         j = i - 1;
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &j);
+        fprintf(fitxer, "%d %p\n", 2, &j);
+
+        fprintf(fitxer, "%d %p\n", 2, &j);
+        fprintf(fitxer, "%d %p\n", 2, &nombres[j]);
+        fprintf(fitxer, "%d %p\n", 2, &key);
         while(j >= 0 && nombres[j] > key)
         {
             nombres[j+1] = nombres[j];
-            j--; 
+            fprintf(fitxer, "%d %p\n", 2, &j);
+            fprintf(fitxer, "%d %p\n", 2, &nombres[j]);
+            fprintf(fitxer, "%d %p\n", 2, &j);
+            fprintf(fitxer, "%d %p\n", 3, &nombres[j+1]);
+
+            j--;
+            fprintf(fitxer, "%d %p\n", 2, &j);
+            fprintf(fitxer, "%d %p\n", 3, &j);
+
+            fprintf(fitxer, "%d %p\n", 2, &j);
+            fprintf(fitxer, "%d %p\n", 2, &nombres[j]);
+            fprintf(fitxer, "%d %p\n", 2, &key);
         }
         nombres[j+1] = key;
+        fprintf(fitxer, "%d %p\n", 2, &key);
+        fprintf(fitxer, "%d %p\n", 2, &j);
+        fprintf(fitxer, "%d %p\n", 3, &nombres[j+1]);
+
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &i);
+        fprintf(fitxer, "%d %p\n", 2, &n);
     }
 }
 
@@ -73,15 +119,41 @@ int agafar_primers()
 {
     int i, j;
     j = 0;
+    fprintf(fitxer, "%d %p\n", 3, &j);
+
+    fprintf(fitxer, "%d %p\n", 3, &i);
     for(i = 0; i < MAX; i++)
     {
-        if((j == 0 || nombres[i] != primers[j-1]) && es_primer(nombres[i]))
+        if((equal_to_0(j) || mirar_anterior(i, j)) && es_primer(nombres[i]))
         {
             primers[j] = nombres[i];
+            fprintf(fitxer, "%d %p\n", 2, &nombres[i]);
+            fprintf(fitxer, "%d %p\n", 3, &primers[i]);
             j++;
+            fprintf(fitxer, "%d %p\n", 2, &j);
+            fprintf(fitxer, "%d %p\n", 3, &j);
+
         }
+
+        fprintf(fitxer, "%d %p\n", 2, &i);
     }
     return j;
+    fprintf(fitxer, "%d %p\n", 2, &j);
+}
+
+int mirar_anterior(int i, int j)
+{
+    fprintf(fitxer, "%d %p\n", 2, &j);
+    fprintf(fitxer, "%d %p\n", 2, &i);
+    fprintf(fitxer, "%d %p\n", 2, &primers[j-1]);
+    fprintf(fitxer, "%d %p\n", 2, &nombres[i]);
+    return nombres[i] != primers[j-1];
+}
+
+int equal_to_0(int j)
+{
+    fprintf(fitxer, "%d %p\n", 2, &j);
+    return j == 0;
 }
 
 int es_primer(int num)
@@ -100,18 +172,39 @@ int es_primer(int num)
 void claus_criptografiques(int num_max)
 {
     int i;
+    fprintf(fitxer, "%d %p\n", 3, &i);
+    fprintf(fitxer, "%d %p\n", 2, &num_max);
     for(i = 0; i < num_max; i++)
     {
-        clau_cripto[i] = primers[i] * primers[num_max - i -1];
+        clau_cripto[i] = primers[i] * primers[num_max - i - 1];
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 2, &num_max);
+        fprintf(fitxer, "%d %p\n", 2, &primers[num_max - i - 1]);
+        fprintf(fitxer, "%d %p\n", 2, &primers[i]);
+        fprintf(fitxer, "%d %p\n", 2, &clau_cripto[i]);
+
+
+
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &i);
+        fprintf(fitxer, "%d %p\n", 2, &num_max);
     }
 }
 
 void show(int num_max, int numbers[])
 {
     int i;
+    fprintf(fitxer, "%d %p\n", 3, &i);
+    fprintf(fitxer, "%d %p\n", 2, &num_max);
     for(i = 0; i < num_max; i++)
     {
         printf("%i ", numbers[i]);
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 2, &numbers[i]);
+
+        fprintf(fitxer, "%d %p\n", 2, &i);
+        fprintf(fitxer, "%d %p\n", 3, &i);
+        fprintf(fitxer, "%d %p\n", 2, &num_max);
     }
     printf("\n");
 }
